54f707d533b55ab4b0de8683e49ed38c5c82dc73
abnud1 <ahmad13932013@hotmail.com>
2021-10-10 22:37:55 +0300

    move on to using ESM modules instead of executing regular scripts

--- src/ssr.rs
+++ src/ssr.rs
@@ -1,6 +1,7 @@
+use std::convert::TryFrom;
+
 use rusty_v8 as v8;
-use std::collections::HashMap;
-use v8::{Context, Global, Local, OwnedIsolate};
+use v8::{script_compiler::Source, Context, Global, OwnedIsolate};
 
 pub struct SsrEngine {
     isolate: OwnedIsolate,
@@ -19,6 +20,32 @@ impl SsrEngine {
         }
 
         lazy_static::initialize(&INIT_PLATFORM);
+    }
+    fn fake_script_origin<'s>(
+        scope: &mut v8::HandleScope<'s>,
+        resource_name_: &str,
+    ) -> v8::ScriptOrigin<'s> {
+        let resource_name = v8::String::new(scope, resource_name_).unwrap();
+        let resource_line_offset = 0;
+        let resource_column_offset = 0;
+        let resource_is_shared_cross_origin = true;
+        let script_id = 123;
+        let source_map_url = v8::String::new(scope, "source_map_url").unwrap();
+        let resource_is_opaque = true;
+        let is_wasm = false;
+        let is_module = true;
+        v8::ScriptOrigin::new(
+            scope,
+            resource_name.into(),
+            resource_line_offset,
+            resource_column_offset,
+            resource_is_shared_cross_origin,
+            script_id,
+            source_map_url.into(),
+            resource_is_opaque,
+            is_wasm,
+            is_module,
+        )
     }
     pub fn new() -> Self {
         let mut isolate = v8::Isolate::new(Default::default());
@@ -44,7 +71,6 @@ impl SsrEngine {
     pub fn render_to_string(
         self: &mut Self,
         source: &str,
-        entry_point: &str,
         params: Option<&str>,
     ) -> Result<String, String> {
         //The isolate rapresente an isolated instance of the v8 engine
@@ -54,78 +80,44 @@ impl SsrEngine {
         let handle_scope = &mut v8::HandleScope::new(isolate);
 
         //A sandboxed execution context with its own set of built-in objects and functions.
-        let context = Local::new(handle_scope, &self.context);
+        let context = v8::Local::new(handle_scope, &self.context);
 
         //Stack-allocated class which sets the execution context for all operations executed within a local scope.
         let scope = &mut v8::ContextScope::new(handle_scope, context);
 
-        let code = v8::String::new(scope, &format!("{};{}", source, entry_point))
-            .expect("Strings are needed");
+        let code = v8::String::new(scope, source).expect("Strings are needed");
+        let code = Source::new(code, Some(&Self::fake_script_origin(scope, "")));
+        let module = v8::script_compiler::compile_module(scope, code)
+            .ok_or("There aren't runnable scripts")?;
+        let scope = &mut v8::HandleScope::new(scope);
+        if matches!(
+            module.instantiate_module(scope, |_, _, _, _| None),
+            Some(true)
+        ) {
+            module
+                .evaluate(scope)
+                .ok_or("exception occured while running module's code")?;
+            let exports: v8::Local<v8::Object> =
+                v8::Local::try_from(module.get_module_namespace()).unwrap();
+            let default_export_name = v8::String::new(scope, "default").unwrap();
+            let default_export = (*exports).get(scope, default_export_name.into()).unwrap();
+            let func: v8::Local<v8::Function> = unsafe { v8::Local::cast(default_export) };
 
-        let script =
-            v8::Script::compile(scope, code, None).ok_or("There aren't runnable scripts")?;
+            //let func: v8::Local<v8::Function> = unsafe { v8::Local::cast(object) };
+            let params: v8::Local<v8::Value> = match v8::String::new(scope, params.unwrap_or("")) {
+                Some(s) => s.into(),
+                None => v8::undefined(scope).into(),
+            };
 
-        let exports = script
-            .run(scope)
-            .ok_or("Missing entry point. Is the bundle exported as a variable?")?;
-        let object = exports.to_object(scope).ok_or("There are no objects")?;
+            let undef = v8::undefined(scope).into();
 
-        let fn_map = Self::create_fn_map(scope, object);
-
-        let params: v8::Local<v8::Value> = match v8::String::new(scope, params.unwrap_or("")) {
-            Some(s) => s.into(),
-            None => v8::undefined(scope).into(),
-        };
-
-        let undef = v8::undefined(scope).into();
-
-        let mut rendered = String::new();
-
-        for key in fn_map.keys() {
-            let result = fn_map[key]
+            let result = func
                 .call(scope, undef, &[params])
                 .ok_or("Are all needed props provided?")?;
-
-            let result = result
-                .to_string(scope)
-                .ok_or("The renderer function should return a string")?;
-
-            rendered = format!("{}{}", rendered, result.to_rust_string_lossy(scope));
+            let rendered = result.to_rust_string_lossy(scope);
+            Ok(rendered)
+        } else {
+            Err("failed to instantiate Module".to_string())
         }
-
-        Ok(rendered)
-    }
-
-    fn create_fn_map<'b>(
-        scope: &mut v8::ContextScope<'b, v8::HandleScope>,
-        object: v8::Local<v8::Object>,
-    ) -> HashMap<String, v8::Local<'b, v8::Function>> {
-        let mut fn_map: HashMap<String, v8::Local<v8::Function>> = HashMap::new();
-
-        if let Some(props) = object.get_own_property_names(scope) {
-            fn_map = Some(props)
-                .iter()
-                .enumerate()
-                .map(|(i, &p)| {
-                    let name = p.get_index(scope, i as u32).unwrap();
-
-                    //A HandleScope which first allocates a handle in the current scope which will be later filled with the escape value.
-                    let mut scope = v8::EscapableHandleScope::new(scope);
-
-                    let func = object.get(&mut scope, name).unwrap();
-
-                    let func = unsafe { v8::Local::<v8::Function>::cast(func) };
-
-                    (
-                        name.to_string(&mut scope)
-                            .unwrap()
-                            .to_rust_string_lossy(&mut scope),
-                        scope.escape(func),
-                    )
-                })
-                .collect();
-        }
-
-        fn_map
     }
 }
